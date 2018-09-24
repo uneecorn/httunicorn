8feaddf2ecc6253f64634abf1cf2129b5dba2b6b
Tyler Mendes de Brito <tyler.brito99@gmail.com>
2018-09-23 23:08:53 -0300

    adding content factory

---
+++ HttUnicorn/Converters/ContentFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+
+namespace HttUnicorn.Converters
+{
+    public static class ContentFactory
+    {
+        public static StringContent CreateContent<T>(
+            T obj,
+            Encoding encoding = null,
+            string mediaType = "application/json"
+        )
+        {
+            try
+            {
+                return new StringContent(Serializer.Serialize(obj), encoding ?? Encoding.UTF8, mediaType);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
