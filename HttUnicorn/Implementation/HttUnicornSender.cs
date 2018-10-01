0684238651bd588e6e88bf577f72f7b8536371a2
Tyler Mendes de Brito <tyler.brito99@gmail.com>
2018-10-01 12:25:54 -0300

    changing the GET way

--- HttUnicorn/Implementation/HttUnicornSender.cs
+++ HttUnicorn/Implementation/HttUnicornSender.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using HttUnicorn.Converters;
+using HttUnicorn.Helpers;
 using HttUnicorn.Interfaces;
 
 namespace HttUnicorn.Implementation
@@ -9,16 +12,21 @@ namespace HttUnicorn.Implementation
     public class HttUnicornSender : IHttUnicornSender
     {
         public string Url { get; private set; }
+        public List<UnicornHeader> Headers { get; private set; }
+        public TimeSpan Timeout { get; private set; }
+        public HttpMethod Method { get; private set; }
 
         readonly HttpClient Client;
 
         public HttUnicornSender()
         {
-            Client = new HttpClient();
+            Headers = new List<UnicornHeader>();
+            //Client = new HttpClient();
         }
         public IHttUnicornSender AddHttpRequestHeader(string name, string value)
         {
-            Client.DefaultRequestHeaders.Add(name, value);
+            //Client.DefaultRequestHeaders.Add(name, value);
+            Headers.Add(new UnicornHeader(name, value));
             return this;
         }
 
@@ -27,6 +35,8 @@ namespace HttUnicorn.Implementation
             Url = url;
             return this;
         }
+
+        #region GET
 
         public async Task<TResponseContent> GetAsync<TResponseContent>()
         {
@@ -41,11 +51,62 @@ namespace HttUnicorn.Implementation
             }
         }
 
+
+        public async Task<string> GetJsonAsync()
+        {
+            try
+            {
+                HttpResponseMessage responseMessage = await GetResponseAsync();
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    string json = await responseMessage.Content.ReadAsStringAsync();
+                    responseMessage.Dispose();
+                    return json;
+                }
+                throw new HttpRequestException(
+                    $"Status Code: {responseMessage.StatusCode}. Reason Phrase: {responseMessage.ReasonPhrase}"
+                );
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<HttpResponseMessage> GetResponseAsync()
+        {
+            try
+            {
+                HttpRequestMessage request = new HttpRequestMessage
+                {
+                    Method = Method,
+                    RequestUri = new Uri(Url)
+                };
+                HttpClient client = new HttpClient();
+                foreach (UnicornHeader header in Headers)
+                {
+                    client.DefaultRequestHeaders.Add(header.Name, header.Value);
+                }
+                HttpResponseMessage responseMessage = await client.SendAsync(request);
+                client.Dispose();
+                request.Dispose();
+                return responseMessage;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #endregion
+
+        #region POST
+
         public async Task<TResponseContent> PostAsync<TResponseContent, TRequestContent>(TRequestContent obj)
         {
             try
             {
-                using (HttpResponseMessage responseMessage = 
+                using (HttpResponseMessage responseMessage =
                     await Client.PostAsync(Url, ContentFactory.CreateContent(obj)))
                 {
                     if (responseMessage.IsSuccessStatusCode)
@@ -63,47 +124,9 @@ namespace HttUnicorn.Implementation
                 throw ex;
             }
         }
+        #endregion
 
-        public IHttUnicornSender SetTimeout(TimeSpan timeout)
-        {
-            Client.Timeout = timeout;
-            return this;
-        }
-
-        public async Task<string> GetJsonAsync()
-        {
-            try
-            {
-                using (HttpResponseMessage responseMessage = await GetResponseAsync())
-                {
-                    if (responseMessage.IsSuccessStatusCode)
-                    {
-                        return await responseMessage.Content.ReadAsStringAsync();
-                    }
-                    throw new HttpRequestException(
-                        $"Status Code: {responseMessage.StatusCode}. Reason Phrase: {responseMessage.ReasonPhrase}"
-                    );
-                }
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        public async Task<HttpResponseMessage> GetResponseAsync()
-        {
-            try
-            {
-                HttpResponseMessage responseMessage = await Client.GetAsync(Url);
-                return responseMessage;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
+        #region PUT
         public async Task<TResponseContent> PutAsync<TResponseContent, TRequestContent>(TRequestContent obj)
         {
             try
@@ -126,7 +149,9 @@ namespace HttUnicorn.Implementation
                 throw ex;
             }
         }
+        #endregion
 
+        #region DELETE
         public async Task<TResponseContent> DeleteAsync<TResponseContent>(object key)
         {
             try
@@ -170,6 +195,22 @@ namespace HttUnicorn.Implementation
             {
                 throw ex;
             }
+        }
+        #endregion
+
+        public IHttUnicornSender SetTimeout(TimeSpan timeout)
+        {
+            Timeout = timeout;
+            return this;
+        }
+
+
+
+
+        public IHttUnicornSender SetHttpMethod(HttpMethod method)
+        {
+            Method = method;
+            return this;
         }
     }
 }
---
+++ HttUnicorn/Interfaces/IHasHttpMethodSetter.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+
+namespace HttUnicorn.Interfaces
+{
+    public interface IHasHttpMethodSetter
+    {
+        IHttUnicornSender SetHttpMethod(HttpMethod method);
+    }
+}
